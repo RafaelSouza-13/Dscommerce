83e0afd59e6ebad6a58d1e82aca444bc72559b6c
Rafael Souza <rafa.nds13@gmail.com>
2024-04-15 11:36:11 -0300

    Implementando a listagem de produtos paginados

--- src/main/java/com/rafa/dscommerce/services/ProductService.java
+++ src/main/java/com/rafa/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.rafa.dscommerce.dto.ProductDTO;
 import com.rafa.dscommerce.entities.Product;
 import com.rafa.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -19,5 +22,12 @@ public class ProductService {
         //Optional<Product> result = repository.findById(1L);
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        //Optional<Product> result = repository.findById(1L);
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
