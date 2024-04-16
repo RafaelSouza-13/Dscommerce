e1db37b5a71e476d83c6be4607c29e3eebe54d3a
Rafael Souza <rafa.nds13@gmail.com>
2024-04-16 11:20:28 -0300

    Adicionando validações aos campos do Produto

--- src/main/java/com/rafa/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/rafa/dscommerce/dto/ProductDTO.java
@@ -1,11 +1,19 @@
 package com.rafa.dscommerce.dto;
 
 import com.rafa.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
     private Long id;
+    @Size(min = 3, max = 80, message = "O campo nome precisa ter de 3 a 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+    @Size(min = 10, message = "Precisa ter no mínimo 10 carateres")
+    @NotBlank
     private String description;
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
