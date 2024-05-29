bc72addeb6bdb8f80ed7a051a8ca9331457d9e3b
Rafael Souza <rafa.nds13@gmail.com>
2024-05-29 11:32:53 -0300

    atualização do projeto para o módulo 5

--- src/main/java/com/rafa/dscommerce/entities/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.rafa.dscommerce.entities;
-
-public enum OrderStatus {
-    WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED;
-}
--- src/main/java/com/rafa/dscommerce/services/exceptions/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.rafa.dscommerce.services.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String msg){
-        super(msg);
-    }
-}
